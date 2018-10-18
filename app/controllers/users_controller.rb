bad6a6ac6cd68d69cc3cb7df0d64d57b08537d88
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-18 15:22:24 -0600

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def create
+  @user = User.create(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  else
+    render :new
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 
 end
--- spec/features/user_can_create_an_account_spec.rb
+++ spec/features/user_can_create_an_account_spec.rb
@@ -4,13 +4,14 @@ describe 'create_an_account' do
   it 'can create an account for users' do
     username = "jordan15"
       visit '/'
+
       click_on "Sign Up to Be a User"
-      save_and_open_page
+
       expect(current_path).to eq(new_user_path)
 
-      fill_in :name, with: username
-      fill_in :email, with: "jordanwa1947@gmail.com"
-      fill_in :password, with: '123456'
+      fill_in "Name", with: username
+      fill_in "Email", with: "jordanwa1947@gmail.com"
+      fill_in "Password", with: '123456'
 
       click_on "Create User"
 
